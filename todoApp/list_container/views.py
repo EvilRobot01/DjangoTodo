16e4582371eb1d24f2d9d9920dc3fae01bccb27c
HaroldDRF <95robots@gmail.com>
2020-10-31 23:16:27 -0700

    forms container created

--- todoApp/list_container/views.py
+++ todoApp/list_container/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from .models import Task, ListContainer
 from django.shortcuts import render, redirect
+from .forms import ListContainerModelForm
 
 # Create your views here.
 
 def index(request):
     list_container = ListContainer.objects.all()
+    #Create new container
+    form = ListContainerModelForm(request.POST or None)
     if request.method == 'POST':
-        title = request.POST.get('title', '')
-        new_list = ListContainer(title=title)
-        new_list.save()  
+        if form.is_valid():
+            form.save(commit=False)
         return redirect('/')
-    return render(request, 'list_container/index.html', {'list_container': list_container})
+    return render(request, 'list_container/index.html', {'list_container': list_container, 'form':form})
+    
 
 def check_contained_items(request, container_id):
     contained_items = Task.objects.all().filter(list_container=container_id)
